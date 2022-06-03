4e10f033c527349c0f7488ebfb39ea14fe7c02ec
HatemGr <hatemgribi@gmail.com>
2022-06-03 14:07:30 +0200

    add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :tennis_frequency, :workout_frequency, :climbing_frequency, :futsal_frequency, :running_frequency, numericality: { only_integer: true }, :greater_than_or_equal_to => 0
+  validates :tennis_experience, :workout_experience, :climbing_experience, :futsal_experience, :running_experience, numericality: { only_integer: true }, :greater_than_or_equal_to => 0
+  validates :climbing_level, :workout_benchpress_weight, :workout_squat_weight, :running_distance, :running_speed, numericality: { only_integer: true }, :greater_than_or_equal_to => 0
+
+
   belongs_to :city
   has_many :sport_users
   has_many :sports, through: :sport_users
