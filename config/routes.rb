99e49f902d7508c4fa97a74479b2faa9bff15736
HatemGr <hatemgribi@gmail.com>
2022-06-03 16:26:03 +0200

    Making static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  root to: 'static_pages#new', as: 'home'
+  resources :static_pages, only: [:index], path: :team, as: 'team'
   
   resources :users do
     resources :suggestions
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_02_172046) do
+ActiveRecord::Schema.define(version: 2022_06_03_141220) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,11 @@ ActiveRecord::Schema.define(version: 2022_06_02_172046) do
 
   create_table "sports", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "static_pages", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
