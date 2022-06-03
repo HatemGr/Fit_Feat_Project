aef3c192a02e76b00ca41d2242349f5e10c91209
HatemGr <hatemgribi@gmail.com>
2022-06-03 15:08:43 +0200

    validations for suggestion model

--- app/models/suggestion.rb
+++ app/models/suggestion.rb
@@ -1,4 +1,14 @@
 class Suggestion < ApplicationRecord
+
+  validates_uniqueness_of :partnera, :scope => [:partnerb]
+  validates_uniqueness_of :partnerb, :scope => [:partnera]
+  validate :check_different_partners
+
   belongs_to :partnera, class_name: "User"
   belongs_to :partnerb, class_name: "User"
+
+  def check_different_partners
+    errors.add(:partnerb, "can't be same users for suggestion") if partnera == partnerb
+    errors.add(:partnera, "suggestion already exist") if Suggestion.find_by(partnera:partnerb, partnerb:partnera)
+  end
 end
